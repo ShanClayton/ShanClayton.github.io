622f873d0887933c8bfd0ed2aab1f34c6f1c836a
Shanaun <sclayton1183@gmail.com>
2018-09-25 00:48:28 -0700

    created switching turns

--- connectfour/js/app.js
+++ connectfour/js/app.js
@@ -2,24 +2,37 @@
 $(function(){
 console.log('testing');
 //Selecting Elements
-let $gameboard = $('#gameboard'); // grabbing the board id
+const $gameboard = $('#gameboard'); // grabbing the board id
 
-let $clearButton = $('#reset-board'); // grabbing reset board id
+const $clearButton = $('#reset-board'); // grabbing reset board id
 
-let $nextTurn = $('p'); // grabbing p tage from index.html
+const $nextTurn = $('p'); // grabbing p tage from index.html
 
 let toggle = true; // global variable set to true
 
-let $circleClass = $('.circle'); // grabbing circle class
+const $circleClass = $('.circle'); // grabbing circle class
 
 // Switiching Turns. Step 3
-const toggleFunction = function(column){
-  console.log(testing);
-  
-}
+ const toggleFunction = function(column) {
+   console.log('testing toggleFunction'); // click listener is working
+
+   if (toggle) {
+     $(column).addClass('pink-token'); // if toggle is true, it will add the class of teal-token to the $newBoardDiv created below
+     $nextTurn.text('Player 1'); // when teal-token is added to the div, the p tag will change color
+     $nextTurn.css('color', '#43C6DB'); // color of p tag changes from pink to teal
+   } else {
+     $(column).addClass('teal-token'); // if toggle is false, it will add the clas of pink
+     $nextTurn.text('Player 2 '); // when teal-token is added to the div, the p tag will change to pinks (player 2 ) turn
+     $nextTurn.css('color', '#FC89AC'); // changing color of the p tag to pink
+   };
+
+     toggle = !toggle; // alternates the turns between teal and  pink
+
+     checkingWins(); // calls the checkingWins function to see who has won
+
 //  creating gameboard. Step 1
-const createGameBoard = function(){
-  for (let i = 0; i < 42; i++) {
+  const createGameBoard = function(){
+    for (let i = 0; i < 42; i++) {
     const $gameDiv = $("<div>"); //createing a div that goes below the div board
     $gameDiv.addClass('circle'); // adding class circle to game div
     $gameDiv.attr('id', i); // adding ids with numbers to the circles
@@ -27,13 +40,7 @@ const createGameBoard = function(){
   };
 
 };
-createGameBoard()l // calling createGameBoard to create the board when page loads
-
-
-
-
-
-
+createGameBoard(); // calling createGameBoard to create the board when page loads
 
 
 
