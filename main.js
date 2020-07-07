a40d26abc8cc490a7916601b321026af624fe203
Shanaun <sclayton1183@gmail.com>
2020-07-07 01:44:05 -0700

    under construction

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-$(document).ready(function (){
-  $('.menu-toggler').on('click', function(){
+$(document).ready(function () {
+  $('.menu-toggler').on('click', function () {
     $(this).toggleClass('open');
     $('.top-nav').toggleClass('open');
   });
   //NAV BAR
-  $('.top-nav .nav-link').on('click', function(){
+  $('.top-nav .nav-link').on('click', function () {
     $('.menu-toggler').removeClass('open');
     $('.top-nav').removeClass('open');
   });
 
-    // SMOOTH SCROLL
-  $('nav a[href*="#"]').on('click', function(){
+  // SMOOTH SCROLL
+  $('nav a[href*="#"]').on('click', function () {
     $('html, body').animate({
-      scrollTop:$($(this).attr('href')).offset().top -100
+      scrollTop: $($(this).attr('href')).offset().top - 100
     }, 2000)
   });
 
   // UP BUTTON
-  $('#up').on('click', function(){
+  $('#up').on('click', function () {
     $('html, body').animate({
-      scrollTop:0
+      scrollTop: 0
     }, 2000)
   });
 
   // AOS
   AOS.init({
-     easing: 'ease',
-     duration: 500,
-     once: true
+    easing: 'ease',
+    duration: 500,
+    once: true
   });
 });
